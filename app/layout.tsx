5a3c9e6ecbffd23b0b4b134014254e70a877c65b
boody <bodymagdy97@gmail.com>
2024-12-26 23:55:34 +0200

    AbdElrahman

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Abdelrahman Dev",
   description: "This is my portfolio",
 };
 
